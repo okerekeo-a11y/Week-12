9afd49be9e68a81c8fd55549863d3d892de09c2e
okerekeo-a11y <okerekeo@students.trinitydc.edu>
2026-04-23 21:08:07 -0400

    Random and Math

---
+++ random_math.py
@@ -0,0 +1,11 @@
+"""
+Ogechukwu Okereke
+CSMCS 111
+Week 12 assignment1
+"""
+import random
+import math
+for i in range(5):
+    num = random.randint(1, 100)
+    square_root = math.sqrt(num)
+    print(f"Number: {num}, Square Root: {square_root}")
