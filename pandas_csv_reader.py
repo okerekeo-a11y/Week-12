aeba82cb4a363f9baf7d0e3623554d7d10520bea
okerekeo-a11y <okerekeo@students.trinitydc.edu>
2026-04-29 15:45:48 -0400

    Pandas

---
+++ pandas_csv_reader.py
@@ -0,0 +1,22 @@
+"""
+Ogechukwu Okereke
+CMSC 111
+Week 12 Assignment 2
+"""
+import pandas as pd
+
+df = pd.read_csv("data.csv", encoding="utf-8")
+
+print(df)
+
+print("Rows:", df.shape[0])
+print("Columns:", df.shape[1])
+
+
+
+
+
+
+
+
+
